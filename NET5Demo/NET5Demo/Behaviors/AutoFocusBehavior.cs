d67e2b2ed4ef05382ceebf745071c5188633843b
jorgemagic <jorgemagic@gmail.com>
2020-11-16 09:11:43 +0100

    - Added colliders

---
+++ NET5Demo/NET5Demo/Behaviors/AutoFocusBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Mathematics;
+
+namespace NET5Demo.Behaviors
+{
+    public class AutoFocusBehavior : Behavior
+    {
+        [BindComponent]
+        protected Transform3D Transform = null;
+
+        [BindComponent]
+        protected Camera3D Camera = null;
+
+        private Ray ray;
+
+        public AutoFocusBehavior()
+            : base(FamilyType.PriorityBehavior)
+        { }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            var from = this.Transform.Position;
+            var dir = this.Transform.WorldTransform.Forward;
+            dir.Normalize();
+
+            this.ray.Position = from;
+            this.ray.Direction = dir;
+
+            var hitResult = this.Managers.PhysicManager3D.RayCast(ref ray, this.Camera.FarPlane);
+            if (hitResult.Succeeded)
+            {
+                this.Camera.FocalDistance = Vector3.Distance(this.ray.Position, hitResult.Point);
+            }
+        }
+    }
+}
---
+++ NET5Demo/NET5Demo/Behaviors/ControlBehavior.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Input.Keyboard;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+
+namespace NET5Demo.Behaviors
+{
+    public class ControlBehavior : Behavior
+    {
+        [BindComponent]
+        private PostProcessingGraphRenderer volume;
+
+        private bool EnabledSSAO;
+        private bool EnabledSSR;
+        private bool EnabledDoF;
+        private bool EnabledBloom;
+        private bool EnabledTonemapping;
+        private PostProcessingNodePortDirectiveType ssaoProperty;
+        private PostProcessingNodePortDirectiveType ssrProperty;
+        private PostProcessingNodePortDirectiveType dofProperty;
+        private PostProcessingNodePortDirectiveType bloomProperty;
+        private PostProcessingNodePortDirectiveType toneProperty;
+
+        protected override bool OnAttached()
+        {
+            var result = base.OnAttached();
+
+            this.EnabledSSAO = true;
+            this.EnabledSSR = true;
+            this.EnabledDoF = true;
+            this.EnabledBloom = true;
+            this.EnabledTonemapping = true;
+
+            var enabledNodes = volume.ppGraph.Description.Nodes.FindAll(n => n.Name == "Enabled");
+
+            // SSAO
+            var ssaoEnabledNode = enabledNodes.First();
+            this.ssaoProperty = ssaoEnabledNode.Inputs[0].Type as PostProcessingNodePortDirectiveType;
+
+            // SSR
+            var ssrEnabledNode = enabledNodes.Skip(1).Take(1).Single();
+            this.ssrProperty = ssrEnabledNode.Inputs[0].Type as PostProcessingNodePortDirectiveType;
+
+            // Dof
+            var dofEnabledNode = enabledNodes.Skip(4).Take(1).Single();
+            this.dofProperty = dofEnabledNode.Inputs[0].Type as PostProcessingNodePortDirectiveType;
+
+            // Bloom
+            var bloomEnabledNode = enabledNodes.Skip(5).Take(1).Single();
+            this.bloomProperty = bloomEnabledNode.Inputs[0].Type as PostProcessingNodePortDirectiveType;
+
+            // Tonemapping
+            var toneEnabledNode = enabledNodes.Skip(6).Take(1).Single();
+            this.toneProperty = toneEnabledNode.Inputs[0].Type as PostProcessingNodePortDirectiveType;
+
+            return result;
+        }
+
+        private void RefreshProperty(PostProcessingNodePortDirectiveType property, bool value)
+        {
+            property.Value = value ? property.Directives[1] : property.Directives[0];
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            var display = this.Managers.RenderManager.ActiveCamera3D.Display;
+            if (display == null)
+            {
+                return;
+            }
+
+            var keyboardDispacher = display.KeyboardDispatcher;
+
+            if (keyboardDispacher == null)
+            {
+                return;
+            }
+
+            if (keyboardDispacher.ReadKeyState(Keys.D1) == ButtonState.Pressing)
+            {
+                this.EnabledSSAO = !this.EnabledSSAO;
+                this.RefreshProperty(this.ssaoProperty, this.EnabledSSAO);
+            }
+
+            if (keyboardDispacher.ReadKeyState(Keys.D2) == ButtonState.Pressing)
+            {
+                this.EnabledSSR = !this.EnabledSSR;
+                this.RefreshProperty(this.ssrProperty, this.EnabledSSR);
+            }
+
+            if (keyboardDispacher.ReadKeyState(Keys.D3) == ButtonState.Pressing)
+            {
+                this.EnabledDoF = !this.EnabledDoF;
+                this.RefreshProperty(this.dofProperty, this.EnabledDoF);
+            }
+
+            if (keyboardDispacher.ReadKeyState(Keys.D4) == ButtonState.Pressing)
+            {
+                this.EnabledBloom = !this.EnabledBloom;
+                this.RefreshProperty(this.bloomProperty, this.EnabledBloom);
+            }
+
+            if (keyboardDispacher.ReadKeyState(Keys.D5) == ButtonState.Pressing)
+            {
+                this.EnabledTonemapping = !this.EnabledTonemapping;
+                this.RefreshProperty(this.toneProperty, this.EnabledTonemapping);
+            }
+        }
+    }
+}
